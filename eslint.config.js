ead0d4948450510eabe6abbef13b517d9bdc951c
Fingertips <developer.ghiantan@gmail.com>
2025-04-17 16:30:19 +0800

    chore: update ESLint config and ignore shadcn generated files
    - Updated ESLint configuration to align with the latest Vite setup and ecosystem.
    - Ignored auto-generated files and folders from shadcn in `.eslintignore` to prevent linting unnecessary code.
    - Ensured compatibility with latest ESLint, TypeScript, and Vite plugin rules.
    
    This reduces noise during linting and improves DX when using shadcn UI components.

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import js from "@eslint/js";
+
+export default tseslint.config(
+  { ignores: ["dist", "./src/components/shadcn/*.{ts,tsx}"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  }
+);
