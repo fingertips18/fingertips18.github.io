bed1537476fdc45e239a9737fd39c3a21e9caa6d
Fingertips <developer.ghiantan@gmail.com>
2025-11-05 21:06:15 +0800

    Fix not found page text spacing

--- apps/admin/src/pages/not-found/page.tsx
+++ apps/admin/src/pages/not-found/page.tsx
@@ -5,12 +5,12 @@ import { Route } from '@/routes/route';
 
 const NotFoundPage = () => {
   return (
-    <section className='size-full flex-center flex-col gap-y-2 lg:gap-y-1.5 leading-tight'>
-      <h6 className='lg:text-lg font-bold'>404 Page Not Found</h6>
+    <section className='size-full flex-center flex-col gap-y-2 lg:gap-y-1 leading-tight'>
+      <h6 className='lg:text-lg font-bold leading-none'>404 Page Not Found</h6>
       <p className='text-xs lg:text-sm text-muted-foreground'>
         Woops! Looks like this page doesn't exist.
       </p>
-      <Button asChild className='rounded-full'>
+      <Button asChild className='rounded-full mt-2'>
         <Link to={Route.root}>Go back</Link>
       </Button>
     </section>
