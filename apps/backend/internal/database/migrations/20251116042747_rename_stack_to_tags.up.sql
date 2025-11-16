120ee5249a0727e00527974a2cd7e3e4b9299bd3
Fingertips <developer.ghiantan@gmail.com>
2025-11-16 12:49:28 +0800

    Simplify migration

--- apps/backend/internal/database/migrations/20251116042747_rename_stack_to_tags.up.sql
+++ apps/backend/internal/database/migrations/20251116042747_rename_stack_to_tags.up.sql
@@ -1,8 +1,3 @@
 -- Rename stack column to tags
 ALTER TABLE project
 RENAME COLUMN stack TO tags;
-
--- Ensure NOT NULL and default are preserved
-ALTER TABLE project
-ALTER COLUMN tags SET NOT NULL,
-ALTER COLUMN tags SET DEFAULT '{}';
